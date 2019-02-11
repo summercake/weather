c803a67fba8803c1073f994ddeba7915a2b088a4
summercake@qq.com <summercake@qq.com>
2019-02-11 02:51:09 -0600

    change weacher.php

--- src/Weather.php
+++ src/Weather.php
@@ -32,17 +32,16 @@ class Weather
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
         $types = [
-            'live'     => 'base',
+            'live' => 'base',
             'forecast' => 'all',
         ];
 
-        // 1. 对 `$format` 与 `$type` 参数进行检查，不在范围内的抛出异常。
         if (!\in_array(\strtolower($format), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: '.$format);
         }
 
-        if (!\in_array(\strtolower($type), $types)) {
-            throw new InvalidArgumentException('Invalid type value(base/all): '.$type);
+        if (!\array_key_exists(\strtolower($type), $types)) {
+            throw new InvalidArgumentException('Invalid type value(live/forecast): '.$type);
         }
 
         // 2. 封装 query 参数，并对空值进行过滤。
