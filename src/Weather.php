d90cc207c4b47fe9241463918527fb4ae9a8defe
summercake@qq.com <summercake@qq.com>
2019-02-11 04:34:50 -0600

    phpunit test success

--- src/Weather.php
+++ src/Weather.php
@@ -27,22 +27,25 @@ class Weather
         $this->guzzleOptions = $options;
     }
 
-    public function getWeather($city, $type = 'live', $format = 'json')
+    public function getWeather($city, string $type = 'base', string $format = 'json')
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
-        $types = [
-            'live'     => 'base',
-            'forecast' => 'all',
-        ];
+        //$types = [
+        //    'live'     => 'base',
+        //    'forecast' => 'all',
+        //];
 
+        // 1. 对 `$format` 与 `$type` 参数进行检查，不在范围内的抛出异常。
         if (!\in_array(\strtolower($format), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: '.$format);
         }
 
-        if (!\in_array(\strtolower($type), $types)) {
+        if (!\in_array(\strtolower($type), ['base', 'all'])) {
             throw new InvalidArgumentException('Invalid type value(base/all): '.$type);
         }
+        $format = \strtolower($format);
+        $type = \strtolower($type);
 
         // 2. 封装 query 参数，并对空值进行过滤。
         $query = array_filter([
