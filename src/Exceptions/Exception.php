a4365538ebb81b5f5f815ebe1c4c64e47dee5d27
summercake <summercake@qq.com>
2019-02-11 10:59:54 +0000

    Apply fixes from StyleCI

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the summercake/weather.
+ *
+ * (c) summercake <summercake@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Summercake\Weather\Exceptions;
 
 class Exception extends \Exception
