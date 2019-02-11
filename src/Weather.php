87b0d2b56c2a90a43719246564714232a3479f94
summercake@qq.com <summercake@qq.com>
2019-02-11 01:24:51 -0600

    add .travis.yml

--- src/Weather.php
+++ src/Weather.php
@@ -8,7 +8,6 @@ use Summercake\Weather\Exceptions\InvalidArgumentException;
 
 class Weather
 {
-
     protected $key;
 
     protected $guzzleOptions = [];
@@ -32,24 +31,29 @@ class Weather
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
+        $types = [
+            'live'     => 'base',
+            'forecast' => 'all',
+        ];
+
         // 1. 对 `$format` 与 `$type` 参数进行检查，不在范围内的抛出异常。
         if (!\in_array(\strtolower($format), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: '.$format);
         }
 
-        if (!\in_array(\strtolower($type), ['base', 'all'])) {
+        if (!\in_array(\strtolower($type), $types)) {
             throw new InvalidArgumentException('Invalid type value(base/all): '.$type);
         }
 
         // 2. 封装 query 参数，并对空值进行过滤。
         $query = array_filter([
-            'key' => $this->key,
-            'city' => $city,
-            'output' => $format,
+            'key'        => $this->key,
+            'city'       => $city,
+            'output'     => $format,
             'extensions' => $type,
         ]);
 
-        try {
+        try{
             // 3. 调用 getHttpClient 获取实例，并调用该实例的 `get` 方法，
             // 传递参数为两个：$url、['query' => $query]，
             $response = $this->getHttpClient()->get($url, [
@@ -64,6 +68,16 @@ class Weather
             // 并将调用异常作为 $previousException 传入。
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
+    }
+
+    public function getLiveWeather($city, $format = 'json')
+    {
+        return $this->getWeather($city, 'base', $format);
+    }
+
+    public function getForecastsWeather($city, $format = 'json')
+    {
+        return $this->getWeather($city, 'all', $format);
     }
 }
 
